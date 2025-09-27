d9d5f6513d5573d32e2b53415d2426fe00e2f478
Your Name <you@example.com>
2025-09-27 09:41:24 +0200

    Va bene

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 //Import libraries
 import express from "express"
 import ejs from "ejs"
-import createClient from "@supabase/supabase-js"
+import {createClient} from "@supabase/supabase-js"
 import multer from "multer"
 
 //Config
 const port = process.env.PORT || 3000
 const debug_host = "localhost"
-const database_url = process.env.DATABASE
-const database_key = process.env.DATABASE_KEY
+const database_url = process.env.DATABASE || "https://nsdgbqozatxvpowkxkav.supabase.co"
+const database_key = process.env.DATABASE_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5zZGdicW96YXR4dnBvd2t4a2F2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1ODkxODY4NCwiZXhwIjoyMDc0NDk0Njg0fQ.0UNL6EQjMnRR2KVUzRGC9fRcPZwPbnaGZvHOdomJbFM"
 const app = express()
 const database = createClient(database_url,database_key)
 const upload = multer({ storage: multer.memoryStorage() });
@@ -20,7 +20,7 @@ app.use(express.json())
 app.use(express.static("sources"))
 
 const auth = (req,res,next) => {
-    const sessionId = req.header.cookie.split("=")[1]
+    const sessionId = req.headers.cookie.split("=")[1]
     if(!sessionId)
         return
     if(!sessions[sessionId])
